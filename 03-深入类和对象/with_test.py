5f904dad37685af8d5e03be877315b1d6e559608
zhagyilig <zhagyilig@gmail.com>
2019-12-28 10:04:23 +0000

    [Add] with.

--- 03-深入类和对象/with_test.py
+++ 03-深入类和对象/with_test.py
@@ -8,9 +8,10 @@
 """
 
 
-# 上下文管理器协议
+# 1. 上下文管理器协议
 
 class Sample:
+	# 定义一个上下文管理器
 	def __enter__(self):
 		# 获取资源
 		print('enter.')
@@ -31,3 +32,28 @@ enter.
 do_something...
 exit.
 """
+
+
+# 2. contextilb简化上下文管理器, 进一步优化 1.
+
+import contextlib
+
+@contextlib.contextmanager
+def file_open(file_name):
+	print('file open...')
+	yield {}   # 一定要定义生成器
+	print('file end...')
+
+
+with file_open('slef_ex.py') as f:
+	print('file processing...')
+
+
+""":return
+file open...
+file processing...
+file end...
+"""
+
+# 总结:
+# 第2种方法简化了第1种方法，代码集中
